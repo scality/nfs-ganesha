b5bc18e7c95d96b342926feebabdc12339f13334
Olivier Garaud <olivier.garaud@scality.com>
2021-10-29 18:55:38 +0200

    Keep pidfile locked while running
    The pidfile needs to stay open otherwise the lock will be released.
    This ensure readers that the pid value written in the file is not stale.
    It also allows to check that the process is not started another time
    with the same pidfile.
    
    Change-Id: I83b0e039764fb505d0def059d6e980468dbdcaa6
    Signed-off-by: Olivier Garaud <olivier.garaud@scality.com>
    (cherry picked from commit ba923ef21449ab688c12fa7a2ad361e37b38b647)

--- src/MainNFSD/nfs_main.c
+++ src/MainNFSD/nfs_main.c
@@ -392,12 +392,14 @@ int main(int argc, char *argv[])
 		if (fcntl(pidfile, F_SETLK, &lk) == -1)
 			LogFatal(COMPONENT_MAIN, "Ganesha already started");
 
-		/* Put pid into file, then close it */
+		/* Put pid into file, then sync it */
 		if (dprintf(pidfile, "%u\n", getpid()) < 0 ||
-		    close(pidfile) < 0)
+		    fsync(pidfile) < 0) {
+			close(pidfile);
 			LogCrit(COMPONENT_MAIN,
 				"Couldn't write pid to file %s error %s (%d)",
 				nfs_pidfile_path, strerror(errno), errno);
+		}
 	}
 
 	/* Set up for the signal handler.
@@ -527,10 +529,14 @@ int main(int argc, char *argv[])
 	if (log_path)
 		free(log_path);
 
+	close(pidfile);
+
 	return 0;
 
 fatal_die:
 	report_config_errors(&err_type, NULL, config_errs_to_log);
+
+	close(pidfile);
 
 	/* systemd journal won't display our errors without this */
 	sleep(1);
